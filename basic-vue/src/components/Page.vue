5d081a0a12afc4010f1a8a0708e64637fe206044
omar okla <omaro@me.com>
2020-03-20 02:17:18 -0400

    Created News Vue utilizing Page component

--- basic-vue/src/components/Page.vue
+++ basic-vue/src/components/Page.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="Page">
-    <h3>On a page</h3>
+    <h3>On a page!</h3>
+    <h4>Which page? {{ pageName }}</h4>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Page',
+  props: {
+    pageName: String,
+  },
 };
 </script>
+
+<style scoped>
+.h4 {
+  color: yellow;
+}
+</style>
