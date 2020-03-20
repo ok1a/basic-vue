97605630f36053a6039ce554ffceac13973b69f7
omar okla <omaro@me.com>
2020-03-20 00:59:20 -0400

    created test components for router util. not rendering. hmm.

--- basic-vue/src/main.js
+++ basic-vue/src/main.js
@@ -1,9 +1,31 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 import 'normalize.css';
 
 Vue.config.productionTip = false;
 
+const PageOne = {
+  template: '<div><h3>This is Page One</h3></div>',
+};
+const PageTwo = {
+  template: '<div><h3>This is Page Two</h3></div>',
+};
+
+const routes = [
+  {
+    path: '/1',
+    component: PageOne,
+  },
+  {
+    path: '/2',
+    component: PageTwo,
+  },
+];
+
+const router = new VueRouter({ routes: routes });
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app');
