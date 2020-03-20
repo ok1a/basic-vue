25df7bcea1f11d785560a5391c1b1d9473f01376
omar okla <omaro@me.com>
2020-03-20 01:20:42 -0400

    Created Navigation component containing router-links to navigate routes. Imported and utilized Navigation component within Header component

--- basic-vue/src/components/Header.vue
+++ basic-vue/src/components/Header.vue
@@ -1,17 +1,21 @@
 <template>
-	<div class="Header">
-		<h1>Header Title</h1>
-	</div>
+  <div class="Header">
+    <h1>Header Title</h1>
+    <Navigation />
+  </div>
 </template>
 
 <script>
+import Navigation from './Navigation';
+
 export default {
-	name: 'Header',
+  name: 'Header',
+  components: { Navigation },
 };
 </script>
 
 <style scoped>
 h1 {
-	color: red;
+  color: red;
 }
 </style>
---
+++ basic-vue/src/components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Navigation">
+    <router-link to="/">Index</router-link>
+    <router-link to="/home">Home</router-link>
+    <router-link to="/news">News</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+};
+</script>
+
+<style scoped></style>
