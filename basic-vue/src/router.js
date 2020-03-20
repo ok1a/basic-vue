083111ce55a2a54f90fdf83c71744bd73ec92c36
omar okla <omaro@me.com>
2020-03-20 02:19:37 -0400

    Utilizing new News vue on /news route

--- basic-vue/src/router.js
+++ basic-vue/src/router.js
@@ -3,21 +3,28 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
+// route imports
+import NewsPage from './views/News.vue';
+
 const Home = {
   template: '<div><h3>Home <Button/></h3></div>',
 };
-const News = {
-  template: '<div><h3>News</h3></div>',
-};
+// const News = {
+//   template: '<div><h3>News</h3></div>',
+// };
 
 const routes = [
   {
     path: '/home',
     component: Home,
   },
+  //   {
+  //     path: '/news',
+  //     component: News,
+  //   },
   {
     path: '/news',
-    component: News,
+    component: NewsPage,
   },
 ];
 
