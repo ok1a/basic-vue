89bccdf444cd012282c0fd5eb9487aeb04a5fa0f
omar okla <omaro@me.com>
2020-03-20 01:30:32 -0400

    Extracted router into new router.js. imported and utilized in vue instance in main.js

--- basic-vue/src/main.js
+++ basic-vue/src/main.js
@@ -1,31 +1,9 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import App from './App.vue';
+import router from './router';
 import 'normalize.css';
 
 Vue.config.productionTip = false;
-
-Vue.use(VueRouter);
-
-const Home = {
-  template: '<div><h3>Home</h3></div>',
-};
-const News = {
-  template: '<div><h3>News</h3></div>',
-};
-
-const routes = [
-  {
-    path: '/home',
-    component: Home,
-  },
-  {
-    path: '/news',
-    component: News,
-  },
-];
-
-const router = new VueRouter({ routes });
 
 new Vue({
   router,
--- utilizations.md
+++ utilizations.md
@@ -5,3 +5,7 @@ normalize.css
 ## resources
 
 [implementing css reset within vue](https://www.educative.io/courses/learn-vuejs-from-scratch/YQW33n6DD0K)
+
+[vuerouter](https://router.vuejs.org/guide/#html)
+	- [runtimecompiler error](https://github.com/vuejs/vue-cli/issues/2754)
+		- [fix](https://cli.vuejs.org/config/#lintonsave)
