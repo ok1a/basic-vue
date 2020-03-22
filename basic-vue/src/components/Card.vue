a446e4c51a97bd5b889d74a427c7a27fabe4a7c2
omar okla <omaro@me.com>
2020-03-22 04:27:20 -0400

    content Card component creation

---
+++ basic-vue/src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <h3>article heading:</h3>
+      <div class="card-content-summary">
+        <p>summary here</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    article: Object,
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid black;
+}
+</style>
