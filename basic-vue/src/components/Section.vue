14ea22506a6c1ced57b07c27e51e57842cca6cce
omar okla <omaro@me.com>
2020-03-22 04:19:20 -0400

    Playing around with list rendering. Working.

--- basic-vue/src/components/Section.vue
+++ basic-vue/src/components/Section.vue
@@ -1,12 +1,30 @@
 <template>
   <section>
     <h1>News</h1>
+    <div>
+      <div v-for="news in newsArr" :key="news.heading">
+        <h2>
+          {{ news.heading }}
+        </h2>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
   name: 'Section',
+  data: function() {
+    return {
+      newsArr: [
+        { heading: 'corona!', article: 'article text 1' },
+        { heading: 'corona!', article: 'article text 1' },
+        { heading: 'corona!', article: 'article text 1' },
+        { heading: 'corona!', article: 'article text 1' },
+        { heading: 'corona!', article: 'article text 1' },
+      ],
+    };
+  },
 };
 </script>
 
@@ -14,5 +32,6 @@ export default {
 section {
   border: 1px solid black;
   height: 100%;
+  padding-bottom: 20rem;
 }
 </style>
