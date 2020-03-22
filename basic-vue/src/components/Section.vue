9a664977dcf08c3080279420bafd3acbd6af8f7a
omar okla <omaro@me.com>
2020-03-22 00:04:34 -0400

    Begin new News component to expect data from API

---
+++ basic-vue/src/components/Section.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>News</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Section',
+};
+</script>
+
+<style scoped>
+section {
+  border: 1px solid black;
+  height: 100%;
+}
+</style>
---
+++ basic-vue/src/views/News2.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="NewsPage">
+		<!-- <Page pageName="News" /> -->
+		<Section />
+	</div>
+</template>
+
+<script>
+// import Page from '../components/Page';
+import Section from '../components/Section';
+
+export default {
+	name: 'NewsPage',
+	components: {
+		// Page,
+		Section,
+	},
+};
+</script>
