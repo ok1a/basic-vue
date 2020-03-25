993040efe47b79ee4b9d504510bbfd871a1749e1
omar okla <omaro@me.com>
2020-03-24 23:20:13 -0400

    added client side form validation and error handling

--- basic-vue/src/components/News/ArticleForm.vue
+++ basic-vue/src/components/News/ArticleForm.vue
@@ -1,9 +1,15 @@
 <template>
 	<div class="article-form-container">
-		<form class="article-form">
+		<form @submit.prevent="addArticle" class="article-form">
+	<p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+      <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+    </ul>
+  </p>
 			<input v-model="article_header" placeholder="article header" />
 			<input v-model="article_description" placeholder="article description" />
-			<button v-on:click.prevent="addArticle">Submit New Article</button>
+			<button type="submit">Submit New Article</button>
 		</form>
 	</div>
 </template>
@@ -15,22 +21,43 @@ export default {
 		return {
 			article_header: '',
 			article_description: '',
+			errors: [],
 		};
 	},
 	methods: {
 		addArticle: function() {
-			const newStoryToAdd = {
-				title: this.article_header,
-				description: this.article_description,
-			};
-			this.$store.dispatch('articles/addStory', newStoryToAdd);
+			if (this.validateForm()) {
+				const newStoryToAdd = {
+					title: this.article_header,
+					description: this.article_description,
+				};
+				this.$store.dispatch('articles/addStory', newStoryToAdd);
 
-			this.clearForm();
+				this.clearForm();
+			} else {
+				null;
+			}
 		},
 
 		clearForm: function() {
 			this.article_header = '';
 			this.article_description = '';
+		},
+		validateForm: function() {
+			this.errors = [];
+			if (this.article_header && this.article_description) {
+				return true;
+			} else {
+				const missingReq = req => {
+					return `Please provide an ${req}`;
+				};
+				if (!this.article_header) {
+					this.errors.push(missingReq('article header'));
+				}
+				if (!this.article_description) {
+					this.errors.push(missingReq('article description'));
+				}
+			}
 		},
 	},
 };
