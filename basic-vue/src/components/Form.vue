6ed054bfe3f91a82e9079d4f042845103e923bab
omar okla <omaro@me.com>
2020-03-24 21:12:57 -0400

    begin Form component for new article creation

---
+++ basic-vue/src/components/Form.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="article-form">
+		<form>
+			<input v-model="article_header" placeholder="article header" />
+			<input v-model="article_description" placeholder="article description" />
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ArticleForm',
+	data: {
+		article_header: '',
+		article_description: '',
+	},
+};
+</script>
